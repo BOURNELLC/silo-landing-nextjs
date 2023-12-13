bb7dc8b7f9a0bc5ad89f5e7a46536466283f96bc
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2023-12-14 05:39:39 +0600

    update2

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,8 +30,8 @@ function Hero() {
                     <div className="flex flex-col items-center justify-center pt-10">
 
                         <div className="flex flex-col items-center justify-center pt-5 ">
-                            <div className="flex flex-col lg:flex-row gap-2 items-center justify-center w-11/11 mx-auto lg:w-full">
-                                <input type="text" placeholder="Enter your email address" className="bg-[#060C18] w-full lg:w-[400px] px-14 py-3 rounded-md border border-sky-700 text-white" />
+                            <div className="flex flex-col lg:flex-row gap-2 items-center justify-center w-11/12 mx-auto lg:w-full">
+                                <input type="text" placeholder="Enter your email address" className="bg-[#060C18] w-full lg:w-[400px] px-5 py-3 rounded-md border border-sky-700 text-white" />
                                 <button className="w-full lg:w-[170px] transition duration-200 hover:bg-sky-500 bg-white hover:text-white text-black px-7 py-3 rounded-md">Get Started</button>
                             </div>
                         </div>
